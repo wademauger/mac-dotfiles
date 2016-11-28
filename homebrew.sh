7ebb51cce29195e9bbb3bfbf7c593d33d6aaf501
Wade Mauger <wade_mauger@intuit.com>
2016-11-28 10:49:44 -0800

    Add non brew packages

--- homebrew.sh
+++ homebrew.sh
@@ -1,3 +1,11 @@
 # Install a few essential brew packages
 brew install cask thefuck libgcrypt neovim watch tree tmux dtrx openssl python3 wget
 
+# Install xCode CLI Tools
+xcode-select --install
+
+# Install RVM (Ruby Version Manager)
+\curl -sSL https://get.rvm.io | bash -s stable --ruby
+
+# Install NVM (Node Version Manager)
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash
